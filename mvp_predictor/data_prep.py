2058abbec77ee76488d6f5d44486af38cc2a5fe3
Jordan <64715176+onionsp@users.noreply.github.com>
2024-07-04 10:50:45 +1000

    exploring Ridge regression and custom error metric

--- mvp_predictor/data_prep.py
+++ mvp_predictor/data_prep.py
@@ -1,1 +1,23 @@
 import pandas as pd
+from sklearn.linear_model import Ridge
+data = pd.read_csv('/Users/jsc/Downloads/player_mvp_stats.csv')
+
+# Data cleaning
+del data["Unnamed: 0"]
+pd.isnull(data).sum()
+data = data.fillna(0)
+data.columns
+
+features = [ 'Age', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P',
+       '3PA', '3P%', '2P', '2PA', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB',
+       'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'Year',
+       'W', 'L', 'W/L%', 'GB', 'PS/G',
+       'PA/G', 'SRS']
+
+train = data[data["Year"] < 2021]
+test = data[data["Year"] == 2021]
+
+# Creating basic Ridge regression model
+reg = Ridge(alpha=0.1)
+reg.fit(train[features], train["Share"])
+predictions = reg.predict(test[features])
